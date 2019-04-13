38b0f80a033e4100bfef0dea8cc40117698e6f17
Gibson Ainge <gainge@gmail.com>
2019-04-13 14:55:17 -0600

    updated extract all script to have a customizeable target directory

--- Testing/extract_all.sh
+++ Testing/extract_all.sh
@@ -2,11 +2,21 @@
 
 
 STUDENT_CODE_DIR="student-code-directories"
+TARGET_DIR=$STUDENT_CODE_DIR # Default output directory
 # SEMESTER="winter2019"
+
+if [[ "$#" -eq 0 ]]; then
+  echo "usage: $0 <ZIP FILE> [TARGET DIRECTORY]"
+	exit 1
+fi
+
+if [[ "$#" -ge 2 ]]; then
+  TARGET_DIR=$2
+fi
 
 for file in "$1"/*.zip;
 do
   echo "Processing: [$file]"
-  ./extract_java.sh $file $STUDENT_CODE_DIR
+  ./extract_java.sh $file $TARGET_DIR
   echo "Done"
 done
