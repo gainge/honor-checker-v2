3d5e160938ed08a30e84f7933ccda7c0348647bd
Gibson Ainge <gainge@gmail.com>
2019-04-18 14:00:56 -0600

    copied download files to main directory

---
+++ download.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Extracts the base URL from a github repository
+extractUrl() {
+	local URL="$1"
+
+	# Make it look mighty fine
+	PARSED=$(echo $URL | sed 's@\(http[s]*:\/\/github\.com\/[a-zA-Z]*\/[a-zA-Z]*\)[\/]*.*@\1@')
+
+	echo $PARSED
+}
+
+
+# Check args
+if [[ "$#" -ne 2 ]]; then
+	echo "usage: $0 <URL> <TARGET DIRECTORY>"
+	exit 1
+fi
+
+# Extract the URL
+URL="$1"
+TARGET_DIR="$2"
+TARGET_DIR=${TARGET_DIR%%/} # Strip off trailing slash
+TEMP="temp"
+
+if [[ ! -d $TARGET_DIR ]]; then
+	mkdir $TARGET_DIR
+fi
+
+# Clean up the URL so it's ready for cloning
+URL=$(extractUrl "$URL")
+
+
+# Download the file to <username>.zip
+USER=$(echo $URL | sed 's@[a-zA-Z]*:\/\/github\.com\/\([a-zA-Z]*\)\/\([a-zA-Z]*\)@\1_\2@' )
+
+# Create a fresh target dir and final java file
+TARGET_DIR="$TARGET_DIR/$USER"
+ALLJAVA="$TARGET_DIR/alljava.txt"
+
+if [[ -f $ALLJAVA ]]; then
+	echo $ALLJAVA
+	exit 0 # Do nothing, it's already been made
+fi
+
+if [[ -d $TARGET_DIR ]]; then
+	rm -rf $TARGET_DIR
+fi
+
+mkdir $TARGET_DIR
+touch $ALLJAVA
+
+
+# Clone the repo into the temp directory
+git clone $URL $TEMP # &>/dev/nul # add to suppress output
+
+if [[ -d $TEMP ]]; then
+	# extract all the sick nasty java files and build the final java file
+	find $TEMP -name '*.java' -not -path "*app/build*" -exec cat {} \; >"$ALLJAVA"
+
+	# Clean up after ourselves
+	rm -rf $TEMP
+
+	echo $ALLJAVA
+else
+	echo "### Download failed! ###"
+fi
---
+++ download_repos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+REPO_DIR="repos"
+
+
+if [[ "$#" -eq 0 ]]
+then
+	echo "usage: $0 <URL Links File> [TARGET DIRECTORY]"
+	exit 1
+fi
+
+URLS="$1"
+
+# Attempt to extract a different target directory
+shift # Shift ops down
+if [[ "$#" -ge 1 ]]; then
+	REPO_DIR="$1"
+fi
+
+# Make sure our directoies are set
+if [[ ! -d "$REPO_DIR" ]]
+then
+    mkdir "$REPO_DIR"
+fi
+
+# Extract each of the urls given in the config file
+while IFS="" read -r URL || [[ -n "$URL" ]]
+do
+  echo -e "\tCloning: [$URL]"
+  FILENAME=$(./download.sh $URL $REPO_DIR)
+  echo -e "\tDone! [$FILENAME]"
+	echo
+done < $URLS
+echo "============================================"
+echo "Repositories downloaded to ./$REPO_DIR/"
+echo "============================================"
