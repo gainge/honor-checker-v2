4cfd6802eaa7807c032e0522cc25e1bf19c5f9b1
Gibson Ainge <gainge@gmail.com>
2019-04-18 12:57:12 -0600

    polised multi repo download script

--- Testing/download_repos.sh
+++ Testing/download_repos.sh
@@ -2,13 +2,19 @@
 REPO_DIR="repos"
 
 
-if [[ "$#" -ne 1 ]]
+if [[ "$#" -eq 0 ]]
 then
-	echo "usage: $0 <URL Links>"
+	echo "usage: $0 <URL Links File> [TARGET DIRECTORY]"
 	exit 1
 fi
 
 URLS="$1"
+
+# Attempt to extract a different target directory
+shift # Shift ops down
+if [[ "$#" -ge 1 ]]; then
+	REPO_DIR="$1"
+fi
 
 # Make sure our directoies are set
 if [[ ! -d "$REPO_DIR" ]]
@@ -16,11 +22,14 @@ then
     mkdir "$REPO_DIR"
 fi
 
+# Extract each of the urls given in the config file
 while IFS="" read -r URL || [[ -n "$URL" ]]
 do
-  echo "Downloading: [$URL]"
-  FILENAME=$(sh ./download.sh $URL)
-  mv $FILENAME "$REPO_DIR/$FILENAME"
-  echo "Done!"
+  echo -e "\tCloning: [$URL]"
+  FILENAME=$(./download.sh $URL $REPO_DIR)
+  echo -e "\tDone! [$FILENAME]"
+	echo
 done < $URLS
+echo "============================================"
 echo "Repositories downloaded to ./$REPO_DIR/"
+echo "============================================"
