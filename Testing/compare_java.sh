cddff39e0bd2f1e8e7dfe0c092ee02bd2d1f4300
Gibson Ainge <gainge@gmail.com>
2019-04-18 09:05:59 -0600

    refactored comparing of java files into helper file

---
+++ Testing/compare_java.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# Handle some sick arguments
+c=false
+v=false
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+      -v|--verbose)
+      v=true
+      shift # past flag I guess
+      ;;
+      -c|--count)
+      c=true
+      shift # past flag again
+      ;;
+      *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# Check usage
+if [[ "$#" -ne 2 ]]; then
+  echo "USAGE: $0 [-c -v] FILE PATTERNS"
+fi
+
+# Read in arguments
+FILE=$1
+PATTERNS=$2
+
+if [[ "$c" == false ]] || [[ "$v" == true ]]; then
+  # echo $(grep -i -F -o -f $PATTERNS $FILE | sort | uniq -c | sed 's/^ *//')
+  # grep -i -F -o -f $PATTERNS $FILE | sort | uniq -c
+  grep -i -F -o -f $PATTERNS $FILE | sort | uniq -c | sed 's/^ *.[[:blank:]]//'
+fi
+
+if [[ "$c" == true ]] || [[ "$v" == true ]]; then
+  echo $(grep -i -F -o -f $PATTERNS $FILE | sort | uniq -c | sed 's/^ *//' | wc -l)
+fi
