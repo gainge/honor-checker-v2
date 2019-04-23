2a0509feca335304b50c1e62b37fcd305795ad8e
Gibson Ainge <gainge@gmail.com>
2019-04-23 08:43:46 -0600

    improved cleaning script to make use of exact line matches to remove more specific noise

--- clean_java.sh
+++ clean_java.sh
@@ -2,6 +2,7 @@
 
 # This is where we remove a bunch of gunk in the java files
 NOISE_PATTERNS="noise.txt"
+LINE_NOISE="line_noise.txt"
 
 if [[ "$#" -lt 2 ]]; then
   echo "usage: $0 <SOURCE FILE> <DESTINATION FILE>"
@@ -15,6 +16,7 @@ if [[ "$#" -eq 3 ]]; then
   PARENT_DIR="$3"
   PARENT_DIR=${PARENT_DIR%/[^/]*}
   NOISE_PATTERNS="$PARENT_DIR/$NOISE_PATTERNS" # Kind of ghetto navigate back but w/e
+  LINE_NOISE="$PARENT_DIR/$LINE_NOISE"
 fi
 
 # Make a backup of the input file
@@ -22,16 +24,8 @@ cp $FILE $DESTINATION_FILE
 
 # Perform the necessary replacements
 sed -i 's/[{}]//g' $DESTINATION_FILE                              # Remove brackets
-sed -i 's/^[[:blank:]]*\(.*\)$/\1/' $DESTINATION_FILE             # Remove leading/trailing whitespace
+sed -i 's/^[[:blank:]]*\(.*\)[[:blank:]]*$/\1/' $DESTINATION_FILE             # Remove leading/trailing whitespace
 sed -i 's/^[[:blank:]]*\*[[:blank:]]\(.*\)/\1/' $DESTINATION_FILE # convert javadoc lines to strings
-sed -i 's/^[[:blank:]]*\*[[:blank:]]*$//' $DESTINATION_FILE       # Remove lines with only asterisks
-sed -i 's/^[[:blank:]]*\/\*[[:blank:]]*$//' $DESTINATION_FILE     # Remove lines with only multiline comment header
-sed -i 's/^[[:blank:]]*static[[:blank:]]*$//' $DESTINATION_FILE   # Remove random static lines I guess (?)
-sed -i 's/^;[[:blank:]]*$//' $DESTINATION_FILE                                # Remove lines with various junk
-sed -i 's/^\/\/[[:blank:]]*$//' $DESTINATION_FILE
-sed -i 's/^);[[:blank:]]*$//' $DESTINATION_FILE
-sed -i 's/^)[[:blank:]]*$//' $DESTINATION_FILE
-sed -i 's/^));[[:blank:]]*$//' $DESTINATION_FILE
 sed -i '/^$/d' $DESTINATION_FILE                                  # Remove blank lines
 sed -i 's/\(.*\)/\L\1/g' $DESTINATION_FILE                        # Lowercase
 
@@ -39,5 +33,8 @@ sed -i 's/\(.*\)/\L\1/g' $DESTINATION_FILE                        # Lowercase
 TEMP="temp.txt"
 grep -i -v -f $NOISE_PATTERNS $DESTINATION_FILE > $TEMP; mv $TEMP $DESTINATION_FILE
 
+# Remove the lame lines
+grep -i -x -v -f $LINE_NOISE $DESTINATION_FILE > $TEMP; mv $TEMP $DESTINATION_FILE
+
 # Finally sort things to make our lives easier
-sort -u $DESTINATION_FILE -o $DESTINATION_FILE
+sort $DESTINATION_FILE | sed "s/[[:blank:]]*$//" | uniq > $TEMP;  mv $TEMP $DESTINATION_FILE
