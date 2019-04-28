be3f37e0d750a85b26303d809bebcae4121fb6c7
Gibson Ainge <gainge@gmail.com>
2019-04-28 13:58:29 -0600

    added source file comparison template

---
+++ source.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>View Source Code</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="res/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="res/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="res/favicon-16x16.png">
+    <link rel="manifest" href="res/site.webmanifest">
+    <link rel="mask-icon" href="res/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="res/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="res/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    
+    <script src="./static/js/jquery-3.3.1.min.js"></script>
+    <script src="./static/js/file_compare.js"></script>
+
+    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/css/prism.css">
+    <link rel="stylesheet" href="static/css/spectre.min.css">
+</head>
+<body>
+    <div id="file-compare-container">
+
+        <div class="file-container" id="file1">
+
+        </div>
+        <div class="file-container" id="file2">
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
