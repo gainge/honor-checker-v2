4088a50fd5acdd18fbe3b80d7536aa731ff4e613
Gibson Ainge <gainge@gmail.com>
2019-04-28 09:37:26 -0600

    added favicon stuff

--- index.php
+++ index.php
@@ -4,7 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CSV Test</title>
+    <title>Honor Checker Results</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="res/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="res/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="res/favicon-16x16.png">
+    <link rel="manifest" href="res/site.webmanifest">
+    <link rel="mask-icon" href="res/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="res/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="res/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
     
     <script src="./static/js/jquery-3.3.1.min.js"></script>
     <script src="./static/js/jquery.tablesorter.min.js"></script>
