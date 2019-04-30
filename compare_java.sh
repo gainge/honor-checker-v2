3999e2df93904666529c285f6b839913ed762cd1
Gibson Ainge <gainge@gmail.com>
2019-04-30 14:34:38 -0600

    added help statement to compare_java script

--- compare_java.sh
+++ compare_java.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 
+showHelp() {
+  echo "options:
+  [-v, --verbose]       Display matches as well as the total count as the last line
+  [-c, --count]         Only display the number of matches found
+
+  [-h, --help]          Show help
+  "
+}
+
 # Handle some sick arguments
 c=false
 v=false
+hlp=false
 POSITIONAL=()
 while [[ $# -gt 0 ]]; do
   key="$1"
@@ -17,17 +27,30 @@ while [[ $# -gt 0 ]]; do
       c=true
       shift # past flag again
       ;;
+      -h|--help)
+      hlp=true
+      shift
+      ;;
       *)    # unknown option
       POSITIONAL+=("$1") # save it in an array for later
       shift # past argument
       ;;
   esac
 done
+
+# Check that help usage baby
+if [[ "$hlp" == true ]]; then
+  showHelp
+  exit 1
+fi
+
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 # Check usage
 if [[ "$#" -ne 2 ]]; then
-  echo "USAGE: $0 [-c -v] <FILE> <PATTERNS>"
+  echo "USAGE: $0 [options] <FILE> <PATTERNS>"
+  showHelp
+  exit 1
 fi
 
 # Read in arguments
